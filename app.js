1ce344f72c85345bfca7b9acff19a52e06b61d58
Marino <marinokolaric@outlook.com>
2017-05-02 21:15:49 +0000

    connected to mongoDB

--- app.js
+++ app.js
@@ -1,39 +1,75 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
 
-var campgrounds = [
-  {name: "Granite Hill", image: "https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/280h/HA1RQCRQJ7.jpg"},
-  {name: "Big Creek", image: "https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/280h/KCYAQXCPFI.jpg"},
-  {name: "Great Lake", image: "https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/280h/SBB7QWZJ6E.jpg"}
-];
+var Campground = mongoose.model("Campground", campgroundSchema);
+
 
 app.get("/", function(req,res){
-  res.render("landing"); 
+	res.render("landing"); 
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req,res){
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else{
+		 res.render("index", {campgrounds: allCampgrounds}); 
+		}
+	});
 
- res.render("campgrounds", {campgrounds: campgrounds}); 
 });
 
+// NEW - show a form
 app.get("/campgrounds/new", function(req, res) {
-   res.render("new.ejs"); 
+	 res.render("new.ejs"); 
 });
 
+// CREATE - make a new campground
 app.post("/campgrounds", function(req, res){
-  var name = req.body.name;
-  var image = req.body.image;
-  var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+	//get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var description = req.body.description;
+	var newCampground = {name: name, image: image, description: description};
+	//create a new campground and save to database
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		}else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+	// find the campground with provided ID
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("show", {campground: foundCampground});
+		}
+	});
+	// render show template with that campground
 });
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("The YelpCamp server has started"); 
+	console.log("The YelpCamp server has started"); 
 });
